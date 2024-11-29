85d84d56471c2c4238f54654721d9a7a2d0b228b
Kevin <189381907+SistemasMGP@users.noreply.github.com>
2024-11-29 10:00:23 -0500

    Creando el primer commit para proyecto-2.py

---
+++ proyecto-2.py
@@ -0,0 +1,12 @@
+print("Segunda practica de Git y GitHub")
+
+def multiplicacion(a,b):
+    resultado = a * b
+    return resultado
+
+def division(a,b):
+    resultado = a/b
+    return resultado
+
+print(multiplicacion(8,5))
+print(division(8,4))
